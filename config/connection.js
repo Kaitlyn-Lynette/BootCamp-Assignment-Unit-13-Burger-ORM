f33f655eb683c13a9909fd1eb87356e92e2c99c0
Kaitlyn Rodriguez <kaitlyn.l.rodriguez@gmail.com>
2020-05-26 15:35:55 -0400

    Adding config

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,21 @@
 // Set up MySQL connection.
 var mysql = require('mysql');
 
-var connection = mysql.createConnection({
-  host: 'localhost',
-  port: 3306,
-  user: 'root',
-  password: 'Senator230#',
-  database: 'burgers_db'
-});
+//If on heroku  use JAWSDB connection 
 
+var connection; 
+
+if(process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'Senator230#',
+    database: 'burgers_db'
+  });
+}
 // Make connection.
 connection.connect(function(err) {
   if (err) {
